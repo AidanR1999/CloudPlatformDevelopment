ed35da5fe3272adba8432b7bfa0102cc726d1ef8
Aidan <aidanr1999@hotmail.com>
2021-03-10 19:35:17 +0000

    create default s3 bucket

--- createS3bucket.py
+++ createS3bucket.py
@@ -1,3 +1,26 @@
+#imports
+import logging
 import boto3
+from botocore.exceptions import ClientError
+import json
 
-s3 = boto3.resource('s3')
+#get app secrets
+file = open('appsecrets.json')
+secrets = json.load(file)
+
+#access S3 api
+s3 = boto3.client('s3')
+
+#create bucket
+bucketName = 'audio-bucket-' + secrets['studentId']
+try:
+    bucket = s3.create_bucket(
+        Bucket=bucketName,
+        CreateBucketConfiguration={
+            'LocationConstraint': 'eu-west-2'
+            }
+        )
+    print(bucketName + ' created successfully')
+except ClientError as e:
+    #log error
+    logging.error(e)
