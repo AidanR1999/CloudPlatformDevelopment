8757885086b3c8d90c21af7b69186c9924a6c18c
Aidan <aidanr1999@hotmail.com>
2021-03-12 14:24:52 +0000

    moved mangers to seperate folder

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 import json
-import s3BucketManager
-import sqsQueueManager
-import dynamoDbManager
+import managers.s3BucketManager as _s3BucketManager
+import managers.sqsQueueManager as _sqsQueueManager
+import managers.dynamoDbManager as _dynamoDbManager
 
 #get student ID from app secrets
 file = open('appsecrets.json')
 secrets = json.load(file)
 studentId = secrets['studentId']
 
-bucket = s3BucketManager.create("audio-bucket-" + studentId)
-queue = sqsQueueManager.create("audio-queue-" + studentId)
-table = dynamoDbManager.create("audio-table-" + studentId)
+bucket = _s3BucketManager.create("audio-bucket-" + studentId)
+queue = _sqsQueueManager.create("audio-queue-" + studentId)
+table = _dynamoDbManager.create("audio-table-" + studentId)
