c63ebd638966f8f401c49d6c5e00468863fe0b10
Aidan <aidanr1999@hotmail.com>
2021-03-12 13:52:05 +0000

    create bucket and queue using index

---
+++ index.py
@@ -0,0 +1,12 @@
+import json
+import s3BucketManager
+import sqsQueueManager
+import dynamoDbManager
+
+#get student ID from app secrets
+file = open('appsecrets.json')
+secrets = json.load(file)
+studentId = secrets['studentId']
+
+bucket = s3BucketManager.create("audio-bucket-" + studentId)
+queue = sqsQueueManager.create("audio-queue-" + studentId)
